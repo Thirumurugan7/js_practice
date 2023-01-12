31c61526248da907b8d62eade07a971adedfd7b2
thirumurugan7 <thiru82003@gmail.com>
2023-01-12 21:50:38 +0530

    added event listeners

--- modals/script.js
+++ modals/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const modals = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+const clickHandler = () => {
+  console.log('clikced');
+  showModal();
+};
+const showModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const HideModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < modals.length; i++) {
+  modals[i].addEventListener('click', clickHandler);
+}
+
+document.querySelector('.close-modal').addEventListener('click', HideModal);
+document.querySelector('.overlay').addEventListener('click', HideModal);
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    HideModal();
+  }
+  console.log(e.key);
+});
