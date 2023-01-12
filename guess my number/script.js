a46b62323d64a10a300fe174a66ad93ed018adb0
thirumurugan7 <thiru82003@gmail.com>
2023-01-12 21:02:46 +0530

    guess my number game

---
+++ guess my number/script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const SecretValue = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = SecretValue;
+let score = 20;
+// document.querySelector('.guess').value
+let highscore = SecretValue;
+console.log(SecretValue);
+const disp = document.querySelector('.message').textContent;
+const scoreHandler = function () {
+  if (score == 0) {
+    document.querySelector('.message').textContent = 'You lost!';
+  }
+};
+const handler = function () {
+  console.log('clicked');
+  let guess = document.querySelector('.guess').value;
+  if (guess == SecretValue) {
+    document.querySelector('.message').textContent = 'You Won!';
+    document.querySelector('.highscore').textContent = highscore;
+    document.querySelector('body').style.backgroundColor = 'blue';
+    document.querySelector('.number').style.width = '30rem';
+  }
+  if (guess > SecretValue) {
+    document.querySelector('.message').textContent = 'Too High!';
+    score--;
+    scoreHandler();
+    document.querySelector('.score').textContent = score;
+  }
+  if (guess < SecretValue) {
+    document.querySelector('.message').textContent = 'Too Low!';
+    score--;
+    scoreHandler();
+    document.querySelector('.score').textContent = score;
+  }
+  console.log(guess, SecretValue);
+};
+
+const check = document
+  .querySelector('.check')
+  .addEventListener('click', handler);
+console.log(disp);
